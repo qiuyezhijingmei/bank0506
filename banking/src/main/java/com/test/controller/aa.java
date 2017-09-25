61922107bc42d07492bd0d5966472b77e5b09b32
Huhuibiao <31205690+Huhuibiao@users.noreply.github.com>
2017-09-25 11:00:42 +0800

    Add files via upload

---
+++ banking/src/main/java/com/test/controller/aa.java
@@ -0,0 +1,10 @@
+package com.test.controller;
+
+/**
+ * Created by huhuibiao on 2017/9/25 0025.
+ */
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("wwewe");
+    }
+}
